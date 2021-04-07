6aecfb1ec3d3ddb949df4eba0b1bcc1c9ed5c3d1
denis-rim <denis.aurorim@gmail.com>
2021-04-07 13:08:22 +0300

    3.3: Phonebook backend step3

--- notes/server/index.js
+++ notes/server/index.js
@@ -37,6 +37,17 @@ app.get("/api/persons", (req, res) => {
   res.json(persons);
 });
 
+app.get("/api/persons/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const person = persons.find((person) => person.id === id);
+
+  if (person) {
+    res.json(person);
+  } else {
+    res.status(404).end();
+  }
+});
+
 const PORT = 3001;
 
 app.listen(PORT, () => {
