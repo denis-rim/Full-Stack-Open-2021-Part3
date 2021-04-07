409f8d49d67ddcc1ef38a1b630a2e2ceedad4d6b
denis-rim <denis.aurorim@gmail.com>
2021-04-07 12:59:39 +0300

    3.2: Phonebook backend step2

--- notes/server/index.js
+++ notes/server/index.js
@@ -1,30 +1,37 @@
 const express = require("express");
 const app = express();
 
-const persons = {
-  persons: [
-    {
-      name: "Arto Hellas",
-      number: "040-123456",
-      id: 1,
-    },
-    {
-      name: "Ada Lovelace",
-      number: "39-44-5323523",
-      id: 2,
-    },
-    {
-      name: "Dan Abramov",
-      number: "12-43-234345",
-      id: 3,
-    },
-    {
-      name: "Mary Poppendieck",
-      number: "39-23-6423122",
-      id: 4,
-    },
-  ],
-};
+app.use(express.json());
+
+const persons = [
+  {
+    name: "Arto Hellas",
+    number: "040-123456",
+    id: 1,
+  },
+  {
+    name: "Ada Lovelace",
+    number: "39-44-5323523",
+    id: 2,
+  },
+  {
+    name: "Dan Abramov",
+    number: "12-43-234345",
+    id: 3,
+  },
+  {
+    name: "Mary Poppendieck",
+    number: "39-23-6423122",
+    id: 4,
+  },
+];
+
+app.get("/api/info", (req, res) => {
+  res.send(`
+   <p>Phonebook has info for ${persons.length} people</p>
+   <p>${new Date().toString()}</p>
+  `);
+});
 
 app.get("/api/persons", (req, res) => {
   res.json(persons);
