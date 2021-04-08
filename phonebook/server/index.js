2e2907e229057266044371d424524fbaff4b0c76
denis-rim <denis.aurorim@gmail.com>
2021-04-08 11:36:24 +0300

    3.11 phonebook full stack

--- phonebook/server/index.js
+++ phonebook/server/index.js
@@ -6,6 +6,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static("build"));
 
 morgan.token("logger", (req) => JSON.stringify(req.body));
 
@@ -105,7 +106,7 @@ app.post("/api/persons", (req, res) => {
   res.json(person);
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
