2e6412cfa035d399bf371ab536c43426a0a55892
denis-rim <denis.aurorim@gmail.com>
2021-04-12 20:14:41 +0300

    3.17*: Phonebook database, step5 and 3.18*: Phonebook database step6

--- phonebook/server/index.js
+++ phonebook/server/index.js
@@ -72,12 +72,28 @@ app.post("/api/persons", (req, res, next) => {
     .catch((error) => next(error));
 });
 
+app.put("/api/persons/:id", (req, res, next) => {
+  const body = req.body;
+
+  const person = {
+    name: body.name,
+    number: body.number,
+  };
+
+  Person.findByIdAndUpdate(req.params.id, person, { new: true })
+    .then((updatedPerson) => {
+      res.json(updatedPerson);
+    })
+    .catch((error) => next(error));
+});
+
 const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: "unknown endpoint" });
 };
 
 const errorHandler = (error, request, response, next) => {
   console.error(error.message);
+  console.log(error);
 
   if (error.name === "CastError") {
     return response.status(400).send({ error: "malformatted id" });
